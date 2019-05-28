83303b9b1907c2e1b8750ec666d5d85093133d05
Matthias Hölzl <tc@xantira.com>
2019-05-28 10:26:47 +0200

    Initial version

---
+++ employee.h
@@ -0,0 +1,38 @@
+// Created by Matthias Hölzl on 2019-05-28.
+//
+
+#ifndef EMPLOYEE_EMPLOYEE_H
+#define EMPLOYEE_EMPLOYEE_H
+
+#include <ostream>
+#include <ctime>
+
+class Name {
+public:
+    explicit Name(int Id);
+
+private:
+    friend std::ostream& operator<<(std::ostream& Stream, const Name& Name);
+    friend class Employee;
+    std::string GivenName;
+    std::string LastName;
+};
+
+std::ostream& operator<<(std::ostream& Stream, const Name& Name);
+
+class Employee {
+public:
+    explicit Employee(int InId);
+
+private:
+    friend std::ostream& operator<<(std::ostream& Stream, const Employee& Employee);
+    int MyId;
+    Name MyName;
+    std::tm Birthday;
+    bool IsBirthday() const;
+    double GetSalary() const;
+};
+
+std::ostream& operator<<(std::ostream& Stream, const Employee& Employee);
+
+#endif //EMPLOYEE_EMPLOYEE_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "employee.h"
+
+int main() {
+    for (int i = 0; i < 5; ++i) {
+        Employee employee{i};
+        std::cout << "We present: " << employee << std::endl;
+    }
+    return 0;
+}
